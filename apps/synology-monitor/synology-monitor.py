5fb74e383f49aa0dc38eccd12bd6a020b200e173
gsiscotty <scott.ftd@gmail.com>
2026-05-20 09:05:56 +0200

    chore(monitors): apply playbook changes (auto)

--- apps/synology-monitor/synology-monitor.py
+++ apps/synology-monitor/synology-monitor.py
@@ -77,7 +77,7 @@ except Exception:
             return False
 
 
-VERSION = "1.6.0-0123"
+VERSION = "1.6.0-0124"
 CONFIG_FILE_MODE = 0o600
 CRON_MARKER = "# synology-monitor.py - do not edit this line manually"
 INTERVAL_MIN = 1
