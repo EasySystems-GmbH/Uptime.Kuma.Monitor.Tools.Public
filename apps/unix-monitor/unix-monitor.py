37c7b3be3de3794d3f485a9e0211d6d2e4230579
gsiscotty <scott.ftd@gmail.com>
2026-06-22 13:04:40 +0200

    chore(monitors): apply playbook changes (auto)

--- apps/unix-monitor/unix-monitor.py
+++ apps/unix-monitor/unix-monitor.py
@@ -85,7 +85,7 @@ except Exception:
             return False
 
 
-VERSION = "1.9.0-0004"
+VERSION = "1.10.0-0001"
 CONFIG_FILE_MODE = 0o600
 CRON_MARKER = "# unix-monitor.py - do not edit this line manually"
 INTERVAL_MIN = 1
